ac62f71d03728155c66f599f20d75e245dffe6be
Ohmmaagosh <phatcharaphol@hotmail.com>
2024-04-18 01:24:33 +0700

    bugfix home

--- front-end/app/src/pages/home.tsx
+++ front-end/app/src/pages/home.tsx
@@ -3,16 +3,23 @@ import { useUserStore } from "@/store/user-store"
 import { useNavigate } from "react-router-dom"
 import { Client } from "@/pages/client"
 import { Admin } from "@/pages/admin"
+import { useEffect } from "react"
 
 export default function Home() {
-	const navigate = useNavigate()
-	const [user] = useUserStore((state) => [state.user])
-	if (!user) {
-		navigate("/login")
-	}
-	return (
-		<HomeLayout>
-			{user?.role === "admin" ? <Admin /> : <Client />}
-		</HomeLayout>
-	)
+  const navigate = useNavigate()
+  const [user] = useUserStore((state) => [state.user])
+
+  useEffect(() => {
+    const validateUser = () => {
+      if (!user) {
+        navigate("/login")
+      }
+    }
+    validateUser()
+  }, [])
+  return (
+    <HomeLayout>
+      {user?.role === "admin" ? <Admin /> : <Client />}
+    </HomeLayout>
+  )
 }
